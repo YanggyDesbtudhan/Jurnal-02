2597870d032c9fef183fae2d01f32707a8ae0260
YanggyDesbtudhan <yanggydesbtudhan@gmail.com>
2020-09-17 20:39:40 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HeloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Yanggy Desbtudhan");
+		System.out.println("NIM: 6706194057");
+		System.out.println("Kelas: 43-04");
+	}
+}
